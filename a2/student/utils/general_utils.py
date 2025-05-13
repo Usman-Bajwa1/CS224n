ab850c0d1e791a5809772e89e7b953870222786e
Usman Bajwa <ua65938@gmail.com>
2025-05-13 21:59:37 +0500

    Initial Assignment 2

--- a2/student/utils/general_utils.py
+++ a2/student/utils/general_utils.py
@@ -47,4 +47,15 @@ def get_minibatches(data, minibatch_size, shuffle=True):
             else _minibatch(data, minibatch_indices)
 
 
+def _minibatch(data, minibatch_idx):
+    return data[minibatch_idx] if type(data) is np.ndarray else [data[i] for i in minibatch_idx]
 
+
+def test_all_close(name, actual, expected):
+    if actual.shape != expected.shape:
+        raise ValueError("{:} failed, expected output to have shape {:} but has shape {:}"
+                         .format(name, expected.shape, actual.shape))
+    if np.amax(np.fabs(actual - expected)) > 1e-6:
+        raise ValueError("{:} failed, expected {:} but value is {:}".format(name, expected, actual))
+    else:
+        print(name, "passed!")
